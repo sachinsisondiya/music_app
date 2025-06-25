f0fc8be8519042aaaa17ce12af27ea63d46e0d0e
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-25 11:53:04 +0530

    addding new features

--- src/screen/Sidebar.jsx
+++ src/screen/Sidebar.jsx
@@ -1,8 +1,18 @@
 import styles from "./Sidebar.module.css";
+import SidebarButton from "./sidebarButton";
 const Sidebar= ()=>{
   return (
     <div className={styles.sidebar}>
-      sidebar
+      <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001877.png" alt="profile picture" className={styles.profile}/>
+
+      <div>
+        <SidebarButton/>
+        <SidebarButton/>
+        <SidebarButton/>
+        <SidebarButton/>
+        <SidebarButton/>
+      </div>
+      <SidebarButton/>
     </div>
   )
 }
---
+++ src/screen/sidebarButton.jsx
@@ -0,0 +1,7 @@
+import '../screen/sidebarButton.css';
+const SidebarButton = ()=>{
+  return (
+    <div className="sidebutton">s</div>
+  )
+}
+export default SidebarButton;
