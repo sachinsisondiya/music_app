0b31fa1379e14e81f8d0c56ab2fc41f2ef07b2a8
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-26 10:36:55 +0530

    add css

--- src/screen/Sidebar.jsx
+++ src/screen/Sidebar.jsx
@@ -1,18 +1,25 @@
 import styles from "./Sidebar.module.css";
 import SidebarButton from "./sidebarButton";
+import { MdLibraryMusic } from "react-icons/md";
+import { MdDynamicFeed } from "react-icons/md";
+import { MdOutlineFavorite } from "react-icons/md";
+import { MdVideoLibrary } from "react-icons/md";
+import { FiTrendingUp } from "react-icons/fi";
+import { LuLogOut } from "react-icons/lu";
 const Sidebar= ()=>{
   return (
     <div className={styles.sidebar}>
       <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001877.png" alt="profile picture" className={styles.profile}/>
 
       <div>
-        <SidebarButton/>
-        <SidebarButton/>
-        <SidebarButton/>
-        <SidebarButton/>
-        <SidebarButton/>
+        <SidebarButton title="Feed" to="/feed" icon={<MdDynamicFeed />}/>
+        <SidebarButton title="Favorites" to="/favorites" icon={<MdOutlineFavorite />}/>
+        <SidebarButton title="Library" to="/library" icon={<MdVideoLibrary />}/>
+        <SidebarButton title="Player" to="/player" icon={<MdLibraryMusic />}/>
+        <SidebarButton title="Trending" to="/trending" icon={<FiTrendingUp />
+}/>
       </div>
-      <SidebarButton/>
+      <SidebarButton title="Logout" to="/logout" icon={<LuLogOut />}/>
     </div>
   )
 }
--- src/screen/sidebarButton.jsx
+++ src/screen/sidebarButton.jsx
@@ -1,7 +1,19 @@
 import '../screen/sidebarButton.css';
-const SidebarButton = ()=>{
+import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import { GrEbay } from 'react-icons/gr';
+const SidebarButton = ({title,to,icon})=>{
   return (
-    <div className="sidebutton">s</div>
+    <Link to={to} className='link'>
+      <IconContext.Provider value={{size:"25px",color:"#E8E8E8"}}>
+        <div className="sidebutton">
+      {icon} {title}
+    </div>
+
+      </IconContext.Provider>
+    
+    </Link>
+    
   )
 }
 export default SidebarButton;
