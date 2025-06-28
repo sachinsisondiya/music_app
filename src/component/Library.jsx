e6ffe25dbf67703040afde8f1c4c91c664e960a4
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-28 11:37:06 +0530

    css in library

--- src/component/Library.jsx
+++ src/component/Library.jsx
@@ -1,6 +1,21 @@
+import { useLocation } from "react-router-dom";
+import "./library.css";
 const Library = ()=>{
+  const location =useLocation();
+  const files = location.state?.files || [];
   return (
-    <div className="screen-container"></div>
+    <div className="screen-container">
+     
+     
+       {files.length > 0 ? (
+        <div className="play-list"> 
+        {files.map((file,i) =>(
+          <div key={i} className="play-card">{file.name}</div>
+        ))}</div>
+
+      ) : (<div> no files uploaded</div>)}  
+     
+    </div>
   )
 }
 export default Library;
