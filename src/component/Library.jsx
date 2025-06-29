059036631b036a1b093803a48e3bd3fef1777b3d
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-29 12:02:27 +0530

    new features add

--- src/component/Library.jsx
+++ src/component/Library.jsx
@@ -1,8 +1,14 @@
 import { useLocation } from "react-router-dom";
 import "./library.css";
+import { MdPlayCircleOutline } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 const Library = ()=>{
   const location =useLocation();
   const files = location.state?.files || [];
+  const navigate = useNavigate();
+  const playMusic = (id)=>{
+    navigate("/player", {state:{id}})
+  }
   return (
     <div className="screen-container">
      
@@ -10,7 +16,16 @@ const Library = ()=>{
        {files.length > 0 ? (
         <div className="play-list"> 
         {files.map((file,i) =>(
-          <div key={i} className="play-card">{file.name}</div>
+          <div key={i} className="play-card">
+            <img src="https://static.vecteezy.com/system/resources/previews/000/546/041/original/music-notes-vector-icon.jpg" alt="music cover potos" className="music-img"/>
+            <p className="music-title">
+              {file.name}
+            </p>
+            <div className="icon-div">
+              <MdPlayCircleOutline className="icon"  onClick={()=>playMusic(file)}  />
+            </div>
+            </div>
+
         ))}</div>
 
       ) : (<div> no files uploaded</div>)}  
