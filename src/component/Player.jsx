e1ec97bb72d22d03f790ed962f7bfe83d5d0f841
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-06-30 11:09:33 +0530

    add some css

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,6 +1,20 @@
+import { useLocation } from "react-router-dom";
 const Player = () =>{
+  const location = useLocation();
+  if(location.state) {console.log(location.state)}
+
+  const playMusic=()=>{
+    console.log()
+  }
   return(
-    <div className="screen-container"></div>
+    <div className="screen-container">
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 export default Player;
