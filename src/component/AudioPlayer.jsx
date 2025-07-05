8b8b65be62916aa8118ac01963810f490b4022d3
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-05 16:20:52 +0530

    make functional

--- src/component/AudioPlayer.jsx
+++ src/component/AudioPlayer.jsx
@@ -2,8 +2,43 @@ import "./AudioPlayer.css"
 import ProgressCircle from "./ProgressCircle";
 import WaveAnimation from "./WaveAnimation";
 import Controls from "./Controls";
+import { useRef,useState,useEffect } from "react";
 const AudioPlayer=({name,isPlaying})=>{
-  console.log(name);
+  const audioRef = useRef(null);
+  const [audioUrl, setAudioUrl] = useState(null);
+
+  const duration= audioRef.current?.duration;
+  console.log(duration);
+  
+ 
+
+
+  useEffect(() => {
+    if (name?.id) {
+      const url = URL.createObjectURL(name.id);
+      setAudioUrl(url);
+      console.log(url)
+
+      return () => {
+        URL.revokeObjectURL(url); // Clean up memory
+      };
+    }
+  }, [name]);
+
+  const handlePlay = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handlePause = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+    }
+  };
+
+
+
   return(
     <div className="player-body flex">
       <div className="player-left-body flex">
@@ -23,8 +58,12 @@ const AudioPlayer=({name,isPlaying})=>{
             <WaveAnimation isPlaying={true}/>
              <p className="duration">0:30</p>
           </div>
-
-            <Controls name={name}/>
+              {audioUrl && (
+        <audio ref={audioRef} src= {audioUrl} />
+      )}
+            <Controls handlePause={handlePause}
+            handlePlay={handlePlay}
+            />
         </div>
         
 
--- src/component/Controls.jsx
+++ src/component/Controls.jsx
@@ -1,45 +1,13 @@
 import { useRef, useEffect, useState } from 'react';
 import './Control.css';
 
-const Controls = ({ name }) => {
-  const audioRef = useRef(null);
-  const [audioUrl, setAudioUrl] = useState(null);
-
-  const duration= audioRef.current?.duration;
-  console.log(duration);
-  
+const Controls = ({handlePause,handlePlay} ) => {
  
-
-
-  useEffect(() => {
-    if (name?.id) {
-      const url = URL.createObjectURL(name.id);
-      setAudioUrl(url);
-      console.log(url)
-
-      return () => {
-        URL.revokeObjectURL(url); // Clean up memory
-      };
-    }
-  }, [name]);
-
-  const handlePlay = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
-  const handlePause = () => {
-    if (audioRef.current) {
-      audioRef.current.pause();
-    }
-  };
+  
 
   return (
     <div className="controls-container">
-      {audioUrl && (
-        <audio ref={audioRef} src={audioUrl} />
-      )}
+     
       <button onClick={handlePlay}>Play</button>
       <button onClick={handlePause}>Pause</button>
     </div>
